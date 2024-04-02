bd96b9fbac27e5afc432a95568e1ce5490f3bcf6
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2024-04-02 11:37:54 +0700

    fix update admin

--- Back End/app.js
+++ Back End/app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 var app = express();
 const server = require('./routes/index')
 const multer = require('multer')
+const cors = require('cors')
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', server.admin)
 app.use('/', server.dashboard)
--- Back End/controllers/admin/dashboard.js
+++ Back End/controllers/admin/dashboard.js
@@ -8,7 +8,7 @@ const dataRuangan = async (req,res) => {
         if (findData.length <= 0) {
             return res.status(400).json({success: false, message: 'Data ruangan belum tersedia'})
         }
-        return res.status(200).json({success:true, message: 'Data ruangan tersedia', daata: findData})        
+        return res.status(200).json({success:true, message: 'Data ruangan tersedia', data: findData})        
     } catch (error) {
         console.log(error)
         return res.status(500).json({success: false, message: 'Kesalahan server'})
